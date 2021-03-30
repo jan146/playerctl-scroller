999352c2004d4f1512e808badc80cd0b4708e998
jan146 <anurjan@gmail.com>
2021-03-30 21:56:25 +0200

    Removed unnecessary dbus call

--- scroller.sh
+++ scroller.sh
@@ -67,28 +67,28 @@ if [ "$1" = "--update" ]; then
 
     DEST=$2
 
-    ARTISTCOMMAND="dbus-send --print-reply \
+    GETMETADATA="dbus-send --print-reply \
     --dest="$DEST" \
     /org/mpris/MediaPlayer2 \
     org.freedesktop.DBus.Properties.Get \
     string:"org.mpris.MediaPlayer2.Player" \
-    string:"Metadata" \
-    | grep -A 2 'artist' \
-    | tail -1 \
-    | sed 's/.*string \"//g;s/.$//g'"
+    string:"Metadata""
 
-    TITLECOMMAND="dbus-send --print-reply \
-    --dest="$DEST" \
-    /org/mpris/MediaPlayer2 \
-    org.freedesktop.DBus.Properties.Get \
-    string:"org.mpris.MediaPlayer2.Player" \
-    string:"Metadata" \
-    | grep -A 1 'title' \
-    | tail -1 \
-    | sed 's/.*string \"//g;s/.$//g'"
+    METADATA=$(eval $GETMETADATA)
 
-    ARTIST=$(eval $ARTISTCOMMAND)
-    TITLE=$(eval $TITLECOMMAND)
+    ARTIST=$(
+        echo "$METADATA" \
+        | grep -A 2 'artist' \
+        | tail -1 \
+        | sed 's/.*string \"//g;s/.$//g'
+    )
+
+    TITLE=$(
+        echo "$METADATA" \
+        | grep -A 1 'title' \
+        | tail -1 \
+        | sed 's/.*string \"//g;s/.$//g'
+    )
     
     if [[ -n "$ARTIST" ]]; then
         echo "$ARTIST$MIDDLE$TITLE" ; exit 0
